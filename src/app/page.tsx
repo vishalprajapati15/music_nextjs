04e26ca1cb116522e78770c37db85dd71dc83a5c
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-02-13 23:17:35 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grig-white/[0.02]">
+      
+      <HeroSection/>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    darkMode: "class",
+    content: [
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                spotlight: "spotlight 2s ease .75s 1 forwards",
+            },
+            keyframes: {
+                spotlight: {
+                    "0%": {
+                        opacity: 0,
+                        transform: "translate(-72%, -62%) scale(0.5)",
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translate(-50%,-40%) scale(1)",
+                    },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
